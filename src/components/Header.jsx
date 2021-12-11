361a294d26c3417f6162f4b92e50c381ddff6f3b
sargam-poudel <sargampoudel100@gmail.com>
2021-12-11 06:22:01 +0545

    mass changes quite a lot improvement and changes

---
+++ src/components/Header.jsx
@@ -0,0 +1,146 @@
+/* This example requires Tailwind CSS v2.0+ */
+import { Fragment, useEffect, useState } from "react";
+import { Popover, Transition } from "@headlessui/react";
+import { MenuIcon, XIcon, CodeIcon, StarIcon } from "@heroicons/react/outline";
+import MediaQuery, { useMediaQuery } from "react-responsive";
+
+const solutions = [
+  {
+    name: "Codes",
+    description:
+      "Get a better understanding of where your traffic is coming from.",
+    href: "#",
+    icon: CodeIcon,
+  },
+  {
+    name: "Favorites",
+    description: "Speak directly to your customers in a more meaningful way.",
+    href: "#",
+    icon: StarIcon,
+  },
+];
+
+const HeaderLarge = () => {
+  const [isLogged, setIsLogged] = useState(false);
+
+  return (
+    <div className="max-w-7xl mx-auto px-4 sm:px-6 bg-gray-900">
+      <div className="flex justify-between items-center border-b-2 border-gray-300 py-6 md:justify-start md:space-x-10">
+        <div className="flex justify-start lg:w-0 lg:flex-1">
+          <a href="/">
+            <span className="sr-only">Workflow</span>
+            <img
+              className="h-8 w-auto sm:h-10"
+              src="https://tailwindui.com/img/logos/workflow-mark-indigo-600.svg"
+              alt="Logo"
+            />
+          </a>
+        </div>
+        <div className="-mr-2 -my-2 md:hidden">
+          <Popover.Button className="bg-indigo-800 rounded-md p-2 inline-flex items-center justify-center text-white hover:bg-indigo-600 hover:text-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500">
+            <span className="sr-only">Open menu</span>
+            <MenuIcon className="h-6 w-6" aria-hidden="true" />
+          </Popover.Button>
+        </div>
+        <Popover.Group as="nav" className="hidden md:flex space-x-10">
+          <a
+            href="#"
+            className="text-base font-medium text-gray-300 hover:text-white"
+          >
+            Codes
+          </a>
+          <a
+            href="#"
+            className="text-base font-medium text-gray-300 hover:text-white"
+          >
+            Favorites
+          </a>
+        </Popover.Group>
+        <div className="hidden md:flex items-center justify-end md:flex-1 lg:w-0">
+          <a
+            href="#"
+            className="whitespace-nowrap text-base font-medium text-gray-300 hover:text-gray-100"
+          >
+            Sign in
+          </a>
+          <a
+            href="#"
+            className="ml-8 whitespace-nowrap inline-flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-base font-medium text-white bg-indigo-700 hover:bg-indigo-600"
+          >
+            Sign up
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+const HeaderMed = () => {
+  return (
+    <Transition
+      as={Fragment}
+      enter="duration-200 ease-out"
+      enterFrom="opacity-0 scale-95"
+      enterTo="opacity-100 scale-100"
+      leave="duration-100 ease-in"
+      leaveFrom="opacity-100 scale-100"
+      leaveTo="opacity-0 scale-95"
+    >
+      <Popover.Panel
+        focus
+        className="absolute top-0 inset-x-0 p-2 transition transform origin-top-right"
+      >
+        <div className="rounded-lg shadow-lg ring-1 ring-black ring-opacity-5 bg-black opacity-100 divide-y-2 divide-gray-50">
+          <div className="pt-5 pb-6 px-5">
+            <div className="flex items-center justify-between">
+              <div>
+                <img
+                  className="h-8 w-auto"
+                  src="https://tailwindui.com/img/logos/workflow-mark-indigo-600.svg"
+                  alt="Workflow"
+                />
+              </div>
+              <div className="-mr-2">
+                <Popover.Button className="bg-gray-900 rounded-md p-2 inline-flex items-center justify-center text-white hover:text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500">
+                  <span className="sr-only">Close menu</span>
+                  <XIcon className="h-6 w-6" aria-hidden="true" />
+                </Popover.Button>
+              </div>
+            </div>
+            <div className="mt-6"></div>
+            <nav className="grid gap-y-8">
+              {solutions.map((item) => (
+                <a
+                  key={item.name}
+                  href={item.href}
+                  className="-m-3 p-3 flex items-center rounded-md  hover:bg-gray-700"
+                >
+                  <item.icon
+                    className="flex-shrink-0 h-6 w-6 text-indigo-600"
+                    aria-hidden="true"
+                  />
+                  <span className="ml-3 text-base font-medium text-gray-300">
+                    {item.name}
+                  </span>
+                </a>
+              ))}
+            </nav>
+          </div>
+        </div>
+      </Popover.Panel>
+    </Transition>
+  );
+};
+
+export default function Header() {
+  const isLarge = useMediaQuery({ query: "(min-width: 768px)" });
+
+  console.log(isLarge);
+  return (
+    <Popover className="relative bg-white">
+      {/* This only gets rendered when the breakpoint allows */}
+      <HeaderLarge />
+      {!isLarge && <HeaderMed />}
+    </Popover>
+  );
+}
---
+++ src/components/Home.jsx
@@ -0,0 +1,35 @@
+import { list } from "postcss";
+import React from "react";
+
+export default function Home() {
+  return (
+    <section>
+      <div className="relative max-w-5xl mx-auto pt-15 text-gray-100 sm:pt-24 lg:pt-32">
+        <h1 className="text-white font-extrabold text-4xl text-center">
+          “share your code, help your friends”
+        </h1>
+        <p className="text-center max-w-5xl mt-5 mx-10">
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Deleniti
+          porro soluta beatae consequuntur non, accusantium nobis numquam fugit
+          culpa. Excepturi quasi rerum perspiciatis hic ratione quam
+          voluptatibus quisquam minima odit?
+        </p>
+
+        <h1 className="text-gray-100 text-center font-extrabold text-4xl p-5">
+          What is this website for?
+        </h1>
+        <p className="text-white text-center mx-10 max-w-5xl">
+          Sharing code has always been a pain in ass for a long time. It is also
+          difficult to teach non-techy guys about version control and github so
+          due to that fact I created this simple site where anyone can post,
+          search and copy code snippits for their benefit
+          <br />
+          Built on good intentions, I hope this webapp will definitely help
+          others and this can be used fairly. Unnecessary api abuse will be
+          definitely considered and even ip ban will be implemented if any sort
+          of api abuse is observed
+        </p>
+      </div>
+    </section>
+  );
+}
