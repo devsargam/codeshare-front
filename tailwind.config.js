5357386b80c5a0ac7efd4f9e2134b7991d3ca195
sargam-poudel <sargampoudel100@gmail.com>
2021-12-17 04:07:16 +0545

    added roboto as the primary font in tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,9 @@ module.exports = {
   content: [],
   purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
   theme: {
-    extend: {},
+    fontFamily: {
+      sans: ["roboto", "sans-serif"],
+    },
   },
   plugins: [],
 };
